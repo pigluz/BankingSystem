bea64e699c382a7e6943c792e6f76086403c5886
robert.brzozowski <noemail>
2023-04-11 15:25:30 +0200

    moved db calls to bank class

--- BankingSystem/Bank.cs
+++ BankingSystem/Bank.cs
@@ -7,17 +7,20 @@ class Bank
 {
     int _remainingAttempts;
     private List<BankAccount> _accounts = new();
-    
+
+    public static string connectionString = @"Server=(localdb)\BankBD;DataBase=Bank";
+    public static SqlConnection connection = new SqlConnection(connectionString);
+
     public void CreateAccount()
     {
         bool validNumber = false;
-        
+
         while (validNumber == false)
         {
             //
             // NO WORKEYYYYYYYYYYYYYYYYYYYYYYYYY....................
             //
-            
+
             Console.WriteLine("Enter a new account number... (2-6 characters long)");
             try
             {
@@ -26,31 +29,35 @@ class Bank
                 // If user enters int correctly...
                 if (!(accountNumber < 10 || accountNumber > 100000))
                 {
-                    var queryIfAccountExisit =
+                    Console.WriteLine("Łączenie z bazą danych...");
+                    using (var connection = new SqlConnection(connectionString))
+                    {
+                        connection.Open();
+                        Console.WriteLine("Połączono.\n");
+                        var queryIfAccountExisit =
                         $"SELECT CASE WHEN EXISTS (SELECT * FROM finanse.bankSystem WHERE accountNumber = '{accountNumber}') THEN CAST (1 AS BIT) ELSE CAST (0 AS BIT) END;";
-                    SqlCommand command = new SqlCommand(queryIfAccountExisit, Program.connection);
-                    int ifAccountExisitResult = (int)command.ExecuteScalar();
+                        SqlCommand command = new SqlCommand(queryIfAccountExisit, connection);
+                        int ifAccountExisitResult = (int)command.ExecuteScalar();
 
-                    // Check if an account with this number already exists...
-                    if (ifAccountExisitResult == 1)
-                    {
-                        Console.WriteLine("An account with this number already exists. Please try again.\n");
+
+                        // Check if an account with this number already exists...
+                        if (ifAccountExisitResult == 1)
+                        {
+                            Console.WriteLine("An account with this number already exists. Please try again.\n");
+                        }
+                        else
+                        {
+                            BankAccount account = new BankAccount();
+                            account.AccountNumber = accountNumber;
+                            account.Balance = 0;
+                            _accounts.Add(account);
+
+                            Console.WriteLine($"Success! Your account with a number {account.AccountNumber} has been created.\n");
+                            validNumber = true;
+                        }
                     }
-                    
                     // If no existing account was found, create a new account...
-                    else
-                    {
-                        BankAccount account = new BankAccount();
-                        account.AccountNumber = accountNumber;
-                        account.Balance = 0;
-                        _accounts.Add(account);
-
-                        Console.WriteLine($"Success! Your account with a number {account.AccountNumber} has been created.\n");
-                        validNumber = true;
-                    }
-                    
                 }
-                
                 // If user enters an int that is shorter than 2 or longer than 6 characters...
                 else
                 {
@@ -58,7 +65,7 @@ class Bank
                     validNumber = false;
                 }
             }
-            
+
             // If user enters value other than int...
             catch (Exception)
             {
@@ -115,14 +122,14 @@ class Bank
                 {
                     validNumber = true;
                     decimal userWithdrawMoney = Convert.ToDecimal(Console.ReadLine());
-                    
+
                     // If user enters a value that is higher than its bank account's balance
                     if (userWithdrawMoney > account.Balance)
                     {
                         Console.WriteLine(
                             $"You entered a value that is higher than balance.\nYour balance is: {account.Balance}\nPlease enter a lower value.\n");
                     }
-                    
+
                     else
                     {
                         account.Balance -= userWithdrawMoney;
@@ -138,14 +145,14 @@ class Bank
             }
         }
     }
-    
+
     public void CheckBalance()
     {
         CheckBankAccount(out var account);
 
         Console.WriteLine($"Your balance is: {account.Balance}\n");
-        
-        
+
+
     }
 
     public void CheckBankAccount(out BankAccount? account)
@@ -165,13 +172,13 @@ class Bank
 
                     account = _accounts.FirstOrDefault(a => a.AccountNumber == userInput);
 
-                    
+
                     // If there is no matching bank account's number...
                     if (account == null)
                     {
                         Console.WriteLine("Wrong number... Please try again.\n");
                         _remainingAttempts--;
-                        
+
                         // If user fails to enter valid accont number...
                         if (_remainingAttempts == 0)
                         {
@@ -183,7 +190,7 @@ class Bank
                             break;
                         }
                     }
-                    
+
                     // If there is a match...
                     else
                     {
@@ -191,14 +198,14 @@ class Bank
                         break;
                     }
                 }
-                
+
                 // If user enter an invalid type of value...
                 catch (Exception)
                 {
                     Console.WriteLine("You entered invalid type of value. Try again...\n");
                     validateBankAccount = false;
                     _remainingAttempts--;
-                    
+
                     // If user fails to enter valid account number../
                     if (_remainingAttempts == 0)
                     {
--- BankingSystem/Program.cs
+++ BankingSystem/Program.cs
@@ -5,61 +5,53 @@ namespace BankingSystem;
 public class Program
 {
     // Serwer o nazwie "BankDB" ; Baza danych o nazwie "Bank" 
-    public static string connectionString = @"Server=(localdb)\BankBD;DataBase=Bank";
-    public static SqlConnection connection = new SqlConnection(connectionString);
+
 
     private static void Main(string[] args)
     {
         var bank = new Bank();
         var exit = false;
 
-        Console.WriteLine("Łączenie z bazą danych...");
-
-        using (var connection = new SqlConnection(connectionString))
+        while (!exit)
         {
-            connection.Open();
-            Console.WriteLine("Połączono.\n");
-
-            while (!exit)
+            Console.WriteLine("Welcome to Test Bank!");
+            while (true)
             {
-                Console.WriteLine("Welcome to Test Bank!");
-                while (true)
+                Console.WriteLine("Menu:\n1.Create Account\n2.Deposit\n3.Withdraw\n4.Check Balance\n5.Exit\n");
+                var userMenuInput = Convert.ToInt32(Console.ReadLine());
+
+                switch (userMenuInput)
                 {
-                    Console.WriteLine("Menu:\n1.Create Account\n2.Deposit\n3.Withdraw\n4.Check Balance\n5.Exit\n");
-                    var userMenuInput = Convert.ToInt32(Console.ReadLine());
+                    case 1:
+                        bank.CreateAccount();
+                        break;
 
-                    switch (userMenuInput)
-                    {
-                        case 1:
-                            bank.CreateAccount();
-                            break;
+                    case 2:
+                        bank.Deposit();
+                        break;
 
-                        case 2:
-                            bank.Deposit();
-                            break;
+                    case 3:
+                        bank.Withdraw();
+                        break;
 
-                        case 3:
-                            bank.Withdraw();
-                            break;
+                    case 4:
+                        bank.CheckBalance();
+                        break;
 
-                        case 4:
-                            bank.CheckBalance();
-                            break;
+                    case 5:
+                        Console.WriteLine("\nExiting Program");
+                        
+                        Environment.Exit(0);
+                        break;
 
-                        case 5:
-                            Console.WriteLine("\nExiting Program");
-                            connection.Close();
-                            Environment.Exit(0);
-                            break;
-
-                        default:
-                            Console.WriteLine("\nWrong value.\nExiting Program");
-                            connection.Close();
-                            Environment.Exit(0);
-                            break;
-                    }
+                    default:
+                        Console.WriteLine("\nWrong value.\nExiting Program");
+                       
+                        Environment.Exit(0);
+                        break;
                 }
             }
         }
+
     }
 }
